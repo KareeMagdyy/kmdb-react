f2e9ded126b442960ec69306d27c5deb2b52a959
KareeMagdyy <karim67899@gmail.com>
2022-07-14 07:36:34 +0200

    added Home Page for guests

---
+++ src/components/MainLoggedOut.jsx
@@ -0,0 +1,33 @@
+import bgImg from "../assets/home-bg-lg.jpg";
+import { Link } from "react-router-dom";
+
+const MainLoggedOut = () => {
+  return (
+    <main>
+      <div className='w-full h-screen absolute'>
+        <img
+          className=' object-cover w-full h-screen absolute'
+          src={bgImg}
+          alt='/'
+        />
+        <div className='bg-black/50 fixed inset-0'></div>
+        <div className=' bg-gradient-to-b from-black/80 fixed inset-0'></div>
+        <div className='fixed flex flex-col gap-4 h-screen justify-center items-center w-full  z-50 text-gray-200'>
+          <h1 className='text-3xl w-[20ch] sm:text-4xl md:text-6xl text-center mx-auto font-black '>
+            Unlimited movies, TV shows, and more.
+          </h1>
+          <h2 className='text-xl font-bold sm:text-2xl md:text-3xl'>
+            Watch anywhere, anytime.
+          </h2>
+          <Link to='/signup'>
+            <button className='bg-red-600 px-10 py-3 rounded mt-4 text-white'>
+              Get Started
+            </button>
+          </Link>
+        </div>
+      </div>
+    </main>
+  );
+};
+
+export default MainLoggedOut;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,28 +1,44 @@
 import requests from "../Requests";
 import Main from "../components/Main";
 import Row from "../components/Row";
+import { UserAuth } from "../context/AuthContext";
+import MainLoggedOut from "../components/MainLoggedOut";
 
 const Home = () => {
+  const { user } = UserAuth();
   return (
     <>
-      <Main />
-      <Row
-        rowId='upcoming'
-        title='Upcoming'
-        fetchURL={requests.requestUpcoming}
-      />
-      <Row
-        rowId='trending'
-        title='Trending'
-        fetchURL={requests.requestTrending}
-      />
-      <Row
-        rowId='toprated'
-        title='TopRated'
-        fetchURL={requests.requestTopRated}
-      />
-      <Row rowId='popular' title='Popular' fetchURL={requests.requestPopular} />
-      <Row rowId='horror' title='Horror' fetchURL={requests.requestHorror} />
+      {!user && <MainLoggedOut />}
+      {user && (
+        <>
+          <Main />
+          <Row
+            rowId='upcoming'
+            title='Upcoming'
+            fetchURL={requests.requestUpcoming}
+          />
+          <Row
+            rowId='trending'
+            title='Trending'
+            fetchURL={requests.requestTrending}
+          />
+          <Row
+            rowId='toprated'
+            title='TopRated'
+            fetchURL={requests.requestTopRated}
+          />
+          <Row
+            rowId='popular'
+            title='Popular'
+            fetchURL={requests.requestPopular}
+          />
+          <Row
+            rowId='horror'
+            title='Horror'
+            fetchURL={requests.requestHorror}
+          />
+        </>
+      )}
     </>
   );
 };
