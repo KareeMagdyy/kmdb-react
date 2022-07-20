809776d2fdd3b85b5df684fc7f976a4de291573a
KareeMagdyy <karim67899@gmail.com>
2022-07-20 05:39:58 +0200

    Biography bug fix

--- src/components/PersonDetails/PersonBiography.jsx
+++ src/components/PersonDetails/PersonBiography.jsx
@@ -13,7 +13,7 @@ const PersonBiography = ({ info }) => {
     <section>
       <div className='text-left container mx-auto'>
         <h2 className='hidden lg:block text-4xl font-bold'>{info?.name}</h2>
-        {info?.biography > 0 && (
+        {info?.biography?.length > 0 && (
           <div className='py-5 w-[85%] mt-2 mx-auto lg:mx-0'>
             <h2 className='text-2xl font-bold'>Biography</h2>
             <div className='bg-red-600 w-[50px] h-[2px] mt-1'></div>
